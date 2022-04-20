f8b954c17ead87ecd54d0d83f093acce77b0c648
Ali Hamidi <57750952+ahmeroxa@users.noreply.github.com>
2022-04-20 10:20:28 -0700

    detect primitive data type
    tests

--- record_test.go
+++ record_test.go
@@ -2,6 +2,7 @@ package turbine
 
 import (
 	"github.com/tidwall/gjson"
+	"log"
 	"testing"
 )
 
@@ -11,13 +12,14 @@ func TestPayload_Set(t *testing.T) {
 		value interface{}
 	}
 	tests := []struct {
-		name    string
-		p       Payload
-		args    args
-		wantErr bool
+		name            string
+		p               Payload
+		args            args
+		wantErr         bool
+		schemaFieldsNum int
 	}{
-		{"add new", []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"users"},"payload":{"id":15}}`), args{"username", "test"}, false},
-		{"update", []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"users"},"payload":{"id":15}}`), args{"id", 16}, false},
+		{"add new", []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"users"},"payload":{"id":15}}`), args{"username", "test"}, false, 2},
+		{"update", []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"users"},"payload":{"id":15}}`), args{"id", 16}, false, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -25,11 +27,10 @@ func TestPayload_Set(t *testing.T) {
 				t.Errorf("Set() error = %v, wantErr %v", err, tt.wantErr)
 			}
 
-			if tt.name == "add new" {
-				schemaFields := gjson.Get(string(tt.p), "schema.fields")
-				if l := len(schemaFields.Array()); l != 2 {
-					t.Errorf("Set() fields len = %v, want %v", l, 2)
-				}
+			schemaFields := gjson.Get(string(tt.p), "schema.fields")
+			if l := len(schemaFields.Array()); l != tt.schemaFieldsNum {
+				log.Printf("p: %+v", string(tt.p))
+				t.Errorf("Set() fields len = %v, want %v", l, tt.schemaFieldsNum)
 			}
 		})
 	}
