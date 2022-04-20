50ea91cf3ae14a940f9a8244a243ae4049fbf5d0
Ali Hamidi <57750952+ahmeroxa@users.noreply.github.com>
2022-04-20 11:54:10 -0700

    fix: correctly handle payload value set (#44)
    * fix: use bool type for optional field
    * detect primitive data type
    * tests

--- record_test.go
+++ record_test.go
@@ -1,6 +1,10 @@
 package turbine
 
-import "testing"
+import (
+	"github.com/tidwall/gjson"
+	"log"
+	"testing"
+)
 
 func TestPayload_Set(t *testing.T) {
 	type args struct {
@@ -8,20 +12,25 @@ func TestPayload_Set(t *testing.T) {
 		value interface{}
 	}
 	tests := []struct {
-		name    string
-		p       Payload
-		args    args
-		wantErr bool
+		name            string
+		p               Payload
+		args            args
+		wantErr         bool
+		schemaFieldsNum int
 	}{
-		{"valid", []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"users"},"payload":{"id":15}}`), args{"username", "test"}, false},
+		{"add new", []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"users"},"payload":{"id":15}}`), args{"username", "test"}, false, 2},
+		{"update", []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"users"},"payload":{"id":15}}`), args{"id", 16}, false, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := tt.p.Set(tt.args.path, tt.args.value); (err != nil) != tt.wantErr {
 				t.Errorf("Set() error = %v, wantErr %v", err, tt.wantErr)
 			}
-			if l := len(tt.p.Get("schema.fields").([]interface{})); l != 2 {
-				t.Errorf("Set() fields len = %v, want %v", l, 2)
+
+			schemaFields := gjson.Get(string(tt.p), "schema.fields")
+			if l := len(schemaFields.Array()); l != tt.schemaFieldsNum {
+				log.Printf("p: %+v", string(tt.p))
+				t.Errorf("Set() fields len = %v, want %v", l, tt.schemaFieldsNum)
 			}
 		})
 	}
