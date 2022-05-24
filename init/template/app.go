ccdb7c982741519e0b8934186c98e4d8c89dad59
Jessy Jordan <jayjayjpg@gmail.com>
2022-05-24 15:46:41 +0200

    fix(app/init): update config code example to avoid syntax errors (#55)

--- init/template/app.go
+++ init/template/app.go
@@ -24,11 +24,12 @@ func (a App) Run(v turbine.Turbine) error {
 	// To configure your data stores as resources on the Meroxa Platform
 	// use the Meroxa Dashboard, CLI, or Meroxa Terraform Provider.
 	// For more details refer to: https://docs.meroxa.com/
-
+	//
 	// Identify an upstream data store for your data app
 	// with the `Resources` function
 	// Replace `source_name` with the resource name the
 	// data store was configured with on Meroxa.
+
 	source, err := v.Resources("source_name")
 	if err != nil {
 		return err
@@ -38,8 +39,11 @@ func (a App) Run(v turbine.Turbine) error {
 	// with the `Records` function
 	// Replace `collection_name` with a table, collection,
 	// or bucket name in your data store.
-	// If a configuration is needed, it would look like
-	// []turbine.ResourceConfig{{Field: "incrementing.field.name", Value: "id"}}
+	// If a configuration is needed for your source,
+	// you can pass it as a second argument to the `Records` function. For example:
+	//
+	// source.Records("collection_name", map[string]interface{"incrementing.field.name":"id"})
+
 	rr, err := source.Records("collection_name", nil)
 	if err != nil {
 		return err
@@ -48,12 +52,14 @@ func (a App) Run(v turbine.Turbine) error {
 	// Specify what code to execute against upstream records
 	// with the `Process` function
 	// Replace `Anonymize` with the name of your function code.
+
 	res := v.Process(rr, Anonymize{})
 
 	// Identify a downstream data store for your data app
 	// with the `Resources` function
 	// Replace `destination_name` with the resource name the
 	// data store was configured with on Meroxa.
+
 	dest, err := v.Resources("destination_name")
 	if err != nil {
 		return err
@@ -63,11 +69,14 @@ func (a App) Run(v turbine.Turbine) error {
 	// using the `Write` function
 	// Replace `collection_archive` with a table, collection,
 	// or bucket name in your data store.
-	// If a configuration is needed you can also use i.e.
+	// If a configuration is needed, you can also use i.e.
+	//
 	// dest.WriteWithConfig(
-	//   res,
-	//   "my-archive",
-	//   []turbine.ResourceConfig{{Field: "buffer.flush.time", Value: "10"}})
+	//  res,
+	//  "my-archive",
+	//  map[string]interface{"buffer.flush.time", Value: "10"}
+	// )
+
 	err = dest.Write(res, "collection_archive")
 	if err != nil {
 		return err
